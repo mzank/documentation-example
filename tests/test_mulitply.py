01e1729124e64683f23d249adb1a4211dbd5ae59
Marco Zank <52705419+mzank@users.noreply.github.com>
2026-04-13 23:33:49 +0200

    test: fix test_multiply.py

--- tests/test_mulitply.py
+++ tests/test_mulitply.py
@@ -7,5 +7,5 @@ sys.path.append(os.path.abspath("src"))
 from multiply import multiply
 
 def test_multiply():
-  assert multiply(3.0, 6.0) = approx(18.0)
+  assert multiply(3.0, 6.0) == approx(18.0)
   
