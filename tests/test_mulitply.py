38cf9de8384e6b47ea56151cd6eaab2d24419769
Marco Zank <52705419+mzank@users.noreply.github.com>
2026-04-15 11:38:58 +0200

    test: add new test for multiply

--- tests/test_mulitply.py
+++ tests/test_mulitply.py
@@ -9,4 +9,5 @@ from multiply import multiply
 def test_multiply():
   assert multiply(3.0, 6.0) == approx(18.0)
   assert multiply(-2.4, 3.5) == approx(-8.4)
+  assert multiply(5.6, -2.8) == approx(-15.68)
   
